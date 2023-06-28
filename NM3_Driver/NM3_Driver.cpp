c0744e9e02f12f7d558f8bc0509c7f41996c0f22
Tom Corner <tom.corner@sonardyne.com>
2023-06-28 11:56:26 +0100

    Modifications to main driver function

--- NM3_Driver/NM3_Driver.cpp
+++ NM3_Driver/NM3_Driver.cpp
@@ -1,20 +1,41 @@
 // NM3_Driver.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define SYSTIMECLOCKHZ 1000000 // sys time clock at 1MHz
+#define PROPTIMECOUNTHZ 16000 // propagation time is a 16kHz counter
+
 #include <iostream>
+#include "modem.h"
+#include "maths.h"
+#include "chirp.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //pass these in as parameters
+    wchar_t COMport[4] = { L'C',L'O',L'M',L'3' };   // check COM port in device manager
+    char modemtype = 'A';                           // A for Alice, B for Bob
+    char epsilon = '5';                             // local ring down guard time in ms
+
+    // these parameters are optional
+    char durationindex = '0';                       // 0: 2ms, 1: 5ms, 2: 10ms, 3: 15ms, 4: 20ms, 5: 25ms, 6: 30ms    
+    char guard = '0';                               // multiple of guard time (5ms) between chirps
+
+    //check modem type is either 'A/a' or 'B/b'
+    // may need to allow for epsilon to be more than one char... check epsilon
+    // if present check duration index and guard are between 0-9 ie 1 char
+
+    Chirp chirpinfo(durationindex, epsilon, guard);
+
+    double test = CounterToMs(4127977, SYSTIMECLOCKHZ);
+    int test1 = MsToCounter(test, SYSTIMECLOCKHZ);
+
+    Modem alice(COMport, 9600);                 // configure alice
+    int propagationtime = alice.Ping(0) / 2;    // retrieve propagation time to bob
+
+    if (propagationtime > -1) {              // no errors
+
+
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
