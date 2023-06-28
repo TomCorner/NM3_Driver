663ea83b968e28ec024739f34d95dcce48dce861
Tom Corner <tom.corner@sonardyne.com>
2023-06-28 11:55:24 +0100

    Added maths functions

---
+++ NM3_Driver/maths.cpp
@@ -0,0 +1,29 @@
+#include "maths.h"
+#include "chirp.h"
+
+//**************************************************************************************************
+//*** Function to calculate the number of chirps that can fit into a propagation delay
+//**************************************************************************************************
+
+int CalculateChirpNumber(double tao, Chirp chirpinfo) {
+	double epsilon = (double)chirpinfo.GetEpsilonVal();	// cast values to doubles before division
+	double duration = (double)chirpinfo.GetDurationVal();
+	double guard = (double)chirpinfo.GetGuardVal();
+	return (int)((tao - epsilon) / (duration + guard));	// cast back to int as can only have an integer number of chirps
+}
+
+//**************************************************************************************************
+//*** Function to convert an int counter of a given clock frequency to time in ms
+//**************************************************************************************************
+
+double CounterToMs(unsigned int count, unsigned int clock) {
+	return ((double)count * (1.0 / (double)clock)) / 1000.0;
+}
+
+//**************************************************************************************************
+//*** Function to convert a time in ms to an int counter of a given clock frequency
+//**************************************************************************************************
+
+int MsToCounter(double ms, unsigned int clock) {
+	return (int)(1000.0 * ms * (double)clock);
+}
---
+++ NM3_Driver/maths.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int CalculateChirpNumber(double tao, Chirp chirpinfo);
+
+double CounterToMs(unsigned int count, unsigned int clock);
+
+int MsToCounter(double ms, unsigned int clock);
