475f5bbc780207660261787d07d2a936624d08ab
Tom Corner <tom.corner@sonardyne.com>
2023-07-05 16:12:24 +0100

    Modifications to main driver including addition of bob function

--- NM3_Driver/NM3_Driver.cpp
+++ NM3_Driver/NM3_Driver.cpp
@@ -34,9 +34,19 @@ int Alice() {
     unsigned messagelength = 0;
 
     Modem alice(COMportnum);                 // configure alice
+    char systimeflag = 'D';                  // 'E' for enabled or 'D' for disabled
 
-    // ****** enable system time
-    int systime = alice.SysTimeEnable();
+    // ****** enable or clear system time 
+    int  systime = alice.SysTimeGet(systimeflag);
+    if (systime < 0) {
+        return -1;
+    }
+    if (systimeflag == 'D') {
+        systime = alice.SysTimeEnable(systimeflag);
+    }
+    else {
+        systime = alice.SysTimeClear(systimeflag);
+    }
     if (systime < 0) {
         return -1;
     }
@@ -46,23 +56,18 @@ int Alice() {
     if (propagationtime < 0) {
         return -1;
     }
-
     double onewaytimems = 0;
     if (propagationtime > 0) {
         onewaytimems = CounterToMs(propagationtime, PROPTIMECOUNTHZ) / 2.0;
     }
     unsigned nrepetitions = CalculateChirpRepetitions(onewaytimems, epsilon, chirpinfo);
-    // ****** Ping bob
 
-    // ****** Get system time and calculate unicast tx time
-    char flag;
-    systime = alice.SysTimeGet(flag);
+    // ****** Get system time and calculate unicast tx time    
+    systime = alice.SysTimeGet(systimeflag);
     if (systime < 0) {
         return -1;
     }
-
     unsigned unitxtime = systime + MsToCounter(alicedelayms, SYSTIMECLOCKHZ);
-    // ****** Get system time and calculate tx time
 
     // ****** Send Chirp info to bob
     if (chirptype == 'U' || chirptype == 'u') {
@@ -74,27 +79,22 @@ int Alice() {
     else {
         cout << "Error: invalid chirp type. Please enter 'U'/'u' for an up-chirp or 'D'/'d' for a down chirp." << "\n";
     }
-
     int txdurationms = alice.SendUnicast(bobaddress, messagebob, messagelength, unitxtime); // add time later...
     if (txdurationms < 0) {
         return -1;
     }
-    // ****** Send Chirp info to bob
-
     std::this_thread::sleep_for(std::chrono::milliseconds(alicedelayms + txdurationms)); // wait for acosutic message to be sent
 
     // ****** Schedule probe signal to bob
     unsigned probetxtime = unitxtime + MsToCounter(onewaytimems, SYSTIMECLOCKHZ) + MsToCounter(bobdelayms, SYSTIMECLOCKHZ) + MsToCounter(txdurationms, SYSTIMECLOCKHZ);
-
     txdurationms = alice.Probe(nrepetitions, chirpinfo, probetxtime);
     if (txdurationms < 0) {
         return -1;
-    }
-    // ****** Schedule probe signal to bob
-    
+    }    
     std::this_thread::sleep_for(std::chrono::milliseconds((unsigned)(onewaytimems + 0.5) + bobdelayms + txdurationms)); // wait for probe to be sent
 
-    systime = alice.SysTimeDisable();
+    // ****** disable system time
+    systime = alice.SysTimeDisable(systimeflag);
     if (systime < 0) {
         return -1;
     }
@@ -102,7 +102,45 @@ int Alice() {
 }
 
 int Bob() {
+    //pass these in as parameters
+    wchar_t COMportnum = L'3';   // check COM port in device manager
+    char modemtype = 'A';                           // A for Alice, B for Bob
+    unsigned epsilon = 5;                             // local ring down guard time in ms (default 5)
 
+    // these parameters are optional (Bob gets them from ALice)
+    //char chirpdurationindex = '0';                       // duration index - see chirp.cpp for values (default 1)
+    //char chirpguardindex = '0';                          // guard index - see chirp.cpp for values    (default 1)
+    //char chirptype = 'U';                                // Alice up (U), bob down (D)
+
+    //Chirp chirpinfo(chirpdurationindex, chirpguardindex, chirptype);
+
+    Modem bob(COMportnum);                 // configure alice
+    char systimeflag = 'D';                  // 'E' for enabled or 'D' for disabled
+
+    // ****** enable or clear system time 
+    int  systime = bob.SysTimeGet(systimeflag);
+    if (systime < 0) {
+        return -1;
+    }
+    if (systimeflag == 'D') {
+        systime = bob.SysTimeEnable(systimeflag);
+    }
+    else {
+        systime = bob.SysTimeClear(systimeflag);
+    }
+    if (systime < 0) {
+        return -1;
+    }
+
+    char rxmessage[1000];
+    systime = bob.UnicastListen(rxmessage);
+
+    // ****** disable system time
+    systime = bob.SysTimeDisable(systimeflag);
+    if (systime < 0) {
+        return -1;
+    }
+    return 0;
 }
 
 void Test() {
@@ -120,7 +158,7 @@ int main()
         Test();
     }
     else {
-        int err = Alice();
+        int err = Bob();
         if (err < 0) {
             std::cout << "\n**** Error in execution. See above messages for clarification ****\n";
         }
