ae4c349d508c1d4bd1b4ff0c2cfb211ffcfd7f6a
Tom Corner <tom.corner@sonardyne.com>
2023-07-18 11:56:59 +0100

    Changes to alice and bob functions for main operation of program

--- NM3_Driver/NM3_Driver.cpp
+++ NM3_Driver/NM3_Driver.cpp
@@ -4,8 +4,8 @@
 #define SYSTIMECLOCKHZ 1000000 // sys time clock at 1MHz
 #define PROPTIMECOUNTHZ 16000 // propagation time is a 16kHz counter
 
-const unsigned alicedelayms = 250;    // in ms
-const unsigned bobdelayms = 500;      // in ms
+const unsigned alicedelayms = 500;    // in ms
+const unsigned bobdelayms = 1500;      // in ms
 
 #include <iostream>
 #include "modem.h"
@@ -13,8 +13,10 @@ const unsigned bobdelayms = 500;      // in ms
 #include "chirp.h"
 #include <chrono>
 #include <thread>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 int Alice() {
     //pass these in as parameters
@@ -52,24 +54,17 @@ int Alice() {
     }
 
     // ****** Ping bob
-    int propagationtime = alice.Ping(bobaddress);
-    if (propagationtime < 0) {
+    int twowaytimecount = alice.Ping(bobaddress);
+    if (twowaytimecount < 0) {
         return -1;
     }
     double onewaytimems = 0;
-    if (propagationtime > 0) {
-        onewaytimems = CounterToMs(propagationtime, PROPTIMECOUNTHZ) / 2.0;
+    if (twowaytimecount > 0) {
+        onewaytimems = CounterToMs(twowaytimecount, PROPTIMECOUNTHZ) / 2.0;
     }
     unsigned nrepetitions = CalculateChirpRepetitions(onewaytimems, epsilon, chirpinfo);
 
-    // ****** Get system time and calculate unicast tx time    
-    systime = alice.SysTimeGet(systimeflag);
-    if (systime < 0) {
-        return -1;
-    }
-    unsigned unitxtime = systime + MsToCounter(alicedelayms, SYSTIMECLOCKHZ);
-
-    // ****** Send Chirp info to bob
+    // ****** prepare Chirp message
     if (chirptype == 'U' || chirptype == 'u') {
         messagelength = sprintf_s(messagebob, "%c%c%02u%c", 'D', chirpinfo.GetDurationChar(), nrepetitions, chirpinfo.GetGuardChar());  // create message
     }
@@ -79,19 +74,27 @@ int Alice() {
     else {
         cout << "Error: invalid chirp type. Please enter 'U'/'u' for an up-chirp or 'D'/'d' for a down chirp." << "\n";
     }
-    int txdurationms = alice.SendUnicast(bobaddress, messagebob, messagelength, unitxtime); // add time later...
+
+    // ****** Get system time and calculate unicast tx time    
+    systime = alice.SysTimeGet(systimeflag);
+    if (systime < 0) {
+        return -1;
+    }
+    unsigned unicasttxtime = systime + MsToCounter(alicedelayms, SYSTIMECLOCKHZ);
+
+    // ****** schedule chirp info message to bob
+    int txdurationms = alice.UnicastWithAck(bobaddress, messagebob, messagelength); // add time later...
     if (txdurationms < 0) {
         return -1;
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(alicedelayms + txdurationms)); // wait for acosutic message to be sent
+    unsigned probetxtime = unicasttxtime + MsToCounter(onewaytimems, SYSTIMECLOCKHZ) + MsToCounter(bobdelayms + 30, SYSTIMECLOCKHZ);
 
-    // ****** Schedule probe signal to bob
-    unsigned probetxtime = unitxtime + MsToCounter(onewaytimems, SYSTIMECLOCKHZ) + MsToCounter(bobdelayms, SYSTIMECLOCKHZ) + MsToCounter(txdurationms, SYSTIMECLOCKHZ);
+    // ****** Schedule probe signal to bob    
     txdurationms = alice.Probe(nrepetitions, chirpinfo, probetxtime);
     if (txdurationms < 0) {
         return -1;
     }    
-    std::this_thread::sleep_for(std::chrono::milliseconds((unsigned)(onewaytimems + 0.5) + bobdelayms + txdurationms)); // wait for probe to be sent
+    std::this_thread::sleep_for(std::chrono::milliseconds(alicedelayms + txdurationms)); // wait for probe to be sent
 
     // ****** disable system time
     systime = alice.SysTimeDisable(systimeflag);
@@ -103,14 +106,15 @@ int Alice() {
 
 int Bob() {
     //pass these in as parameters
-    wchar_t COMportnum = L'3';   // check COM port in device manager
-    char modemtype = 'A';                           // A for Alice, B for Bob
+    wchar_t COMportnum = L'6';   // check COM port in device manager
+    char modemtype = 'B';                           // A for Alice, B for Bob
     unsigned epsilon = 5;                             // local ring down guard time in ms (default 5)
 
     // these parameters are optional (Bob gets them from ALice)
-    //char chirpdurationindex = '0';                       // duration index - see chirp.cpp for values (default 1)
-    //char chirpguardindex = '0';                          // guard index - see chirp.cpp for values    (default 1)
-    //char chirptype = 'U';                                // Alice up (U), bob down (D)
+    char chirpdurationindex = '1';                       // duration index - see chirp.cpp for values (default 1)
+    char chirpguardindex = '1';                          // guard index - see chirp.cpp for values    (default 1)
+    char chirptype = 'D';                                // Alice up (U), bob down (D)
+    unsigned nrepetitions = 0;
 
     //Chirp chirpinfo(chirpdurationindex, chirpguardindex, chirptype);
 
@@ -132,8 +136,21 @@ int Bob() {
         return -1;
     }
 
+    // ****** wait for chirp info from Alice and parse message
     char rxmessage[1000];
     systime = bob.UnicastListen(rxmessage);
+    sscanf_s(rxmessage, "%c%c%2u%c", &chirptype, 1, &chirpdurationindex, 1, &nrepetitions, &chirpguardindex, 1);
+    Chirp chirpinfo(chirpdurationindex, chirpguardindex, chirptype);
+
+    // ****** Schedule probe signal to Alice
+    unsigned probetxtime = systime + MsToCounter(bobdelayms, SYSTIMECLOCKHZ);
+
+    //int testsystime = bob.SysTimeGet(systimeflag);
+    int txdurationms = bob.Probe(nrepetitions, chirpinfo, probetxtime);
+    if (txdurationms < 0) {
+        return -1;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(bobdelayms + txdurationms)); // wait for probe to be sent
 
     // ****** disable system time
     systime = bob.SysTimeDisable(systimeflag);
@@ -144,10 +161,13 @@ int Bob() {
 }
 
 void Test() {
-  
-    
+    steady_clock::time_point t1 = steady_clock::now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    steady_clock::time_point t2 = steady_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+    double diff = time_span.count();
 
-
+    int stop = 0;
 }
 
 int main()
