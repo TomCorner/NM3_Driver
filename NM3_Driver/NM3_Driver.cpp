d5ccb6dcdb27efe76ff428781cf1777675153998
Tom Corner <tom.corner@sonardyne.com>
2023-07-25 17:58:39 +0100

    Modifications to main

--- NM3_Driver/NM3_Driver.cpp
+++ NM3_Driver/NM3_Driver.cpp
@@ -9,34 +9,21 @@
 #define FSOFFSET 30			// in ms
 #define ALICEUNICASTPREP 7  // in ms - for tx encoding
 
-#include "modem.h"
-#include "maths.h"
 #include "chirp.h"
+#include "maths.h"
+#include "modem.h"
 #include <chrono>
 #include <thread>
 
 using namespace std;
 using namespace std::chrono;
 
-int64_t Alice() {
-	//pass these in as parameters
-	wchar_t COMportnum = L'3';   // check COM port in device manager
-	char modemtype = 'A';                           // A for Alice, B for Bob
-	uint16_t epsilon = 5;                             // local ring down guard time in ms (default 5)
+int64_t Alice(Modem alice, uint16_t epsilon, Chirp chirpinfo) {
 
-	// these parameters are optional
-	char chirpdurationindex = '0';                       // duration index - see chirp.cpp for values (default 1)
-	char chirpguardindex = '0';                          // guard index - see chirp.cpp for values    (default 1)
-	char chirptype = 'U';                                // Alice up (U), bob down (D)
-
-	Chirp chirpinfo(chirpdurationindex, chirpguardindex, chirptype);
-
-	uint16_t bobaddress = 102;            // Bobs address
-	char messagebob[64];                // unicast configuration message to bob
+	uint16_t bobaddress = 102;		// Bobs address
+	char messagebob[64];			// unicast configuration message to bob
 	uint16_t messagelength = 0;
-
-	Modem alice(COMportnum);                 // configure alice
-	char systimeflag = 'D';                  // 'E' for enabled or 'D' for disabled
+	char systimeflag = 'D';             // 'E' for enabled or 'D' for disabled
 
 	// ****** Ping bob
 	int64_t twowaytimecount = alice.Ping(bobaddress);
@@ -48,10 +35,10 @@ int64_t Alice() {
 	uint16_t nrepetitions = CalculateChirpRepetitions(onewaytimems, epsilon, chirpinfo);
 
 	// ****** prepare Chirp message
-	if (chirptype == 'U' || chirptype == 'u') {	// TODO - Add this to command line parameter checks as well
+	if (chirpinfo.GetType() == 'U' || chirpinfo.GetType() == 'u') {	// TODO - Add this to command line parameter checks as well
 		messagelength = sprintf_s(messagebob, "%c%c%02u%c", 'D', chirpinfo.GetDurationChar(), nrepetitions, chirpinfo.GetGuardChar());  // create message
 	}
-	else if (chirptype == 'D' || chirptype == 'd') {
+	else if (chirpinfo.GetType() == 'D' || chirpinfo.GetType() == 'd') {
 		messagelength = sprintf_s(messagebob, "%c%c%02u%c", 'U', chirpinfo.GetDurationChar(), nrepetitions, chirpinfo.GetGuardChar());  // create message
 	}
 	else {
@@ -71,7 +58,7 @@ int64_t Alice() {
 	if (txdurationms < 0) {
 		alice.PrintLogs();
 		return txdurationms;
-	}	
+	}
 	uint64_t probetxtime = (unicasttxtime + MsToCounter(onewaytimems, SYSTIMECLOCKHZ) + MsToCounter(BOBDELAYMS, SYSTIMECLOCKHZ) + MsToCounter(FSOFFSET, SYSTIMECLOCKHZ) + MsToCounter(ALICEUNICASTPREP, SYSTIMECLOCKHZ));
 
 	// ****** Schedule probe signal to bob    
@@ -91,22 +78,13 @@ int64_t Alice() {
 	return ENoErr;
 }
 
-int64_t Bob() {
-	//pass these in as parameters
-	wchar_t COMportnum = L'7';   // check COM port in device manager
-	char modemtype = 'B';                           // A for Alice, B for Bob
-	//uint16_t epsilon = 5;                             // local ring down guard time in ms (default 5)
-
+int64_t Bob(Modem bob) {
 	// these parameters are optional (Bob gets them from ALice)
-	char chirpdurationindex = '1';                       // duration index - see chirp.cpp for values (default 1)
-	char chirpguardindex = '1';                          // guard index - see chirp.cpp for values    (default 1)
-	char chirptype = 'D';                                // Alice up (U), bob down (D)
+	char chirpdurationindex = '1';		// duration index - see chirp.cpp for values (default 1)
+	char chirpguardindex = '1';			// guard index - see chirp.cpp for values    (default 1)
+	char chirptype = 'D';				// Alice up (U), bob down (D)
 	uint16_t nrepetitions = 0;
-
-	//Chirp chirpinfo(chirpdurationindex, chirpguardindex, chirptype);
-
-	Modem bob(COMportnum);                 // configure alice
-	char systimeflag = 'D';                // 'E' for enabled or 'D' for disabled
+	char systimeflag = 'D';				// 'E' for enabled or 'D' for disabled
 
 	// ****** enable or clear system time 
 	int64_t systime = bob.SysTimeEnable(systimeflag);
@@ -142,25 +120,45 @@ int64_t Bob() {
 	return ENoErr;
 }
 
-void Test() {
-	steady_clock::time_point t1 = steady_clock::now();
-	this_thread::sleep_for(milliseconds(500));
-	steady_clock::time_point t2 = steady_clock::now();
-	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-	double diff = time_span.count();
-
-	int64_t stop = 0;
-}
-
 int main()
 {
-	bool test = 0;
+	char type = 'A';
+	int64_t err = 0;
 
-	if (test) {
-		Test();
+	if ((type == 'a') || (type == 'A')) {
+		//pass these in as parameters
+		wchar_t COMportnum = L'3';  // check COM port in device manager
+		char modemtype = 'A';		// A for Alice, B for Bob
+		uint16_t epsilon = 5;       // local ring down guard time in ms (default 5)
+		Modem alice(COMportnum, modemtype);	// configure alice
+
+		// these parameters are optional
+		char chirpdurationindex = '1';	// duration index - see chirp.cpp for values (default 1)
+		char chirpguardindex = '1';     // guard index - see chirp.cpp for values    (default 1)
+		char chirptype = 'U';			// Alice up (U), bob down (D)
+		Chirp chirpinfo(chirpdurationindex, chirpguardindex, chirptype);
+
+		while (1) {
+			err = Alice(alice, epsilon, chirpinfo);
+			PrintError(ErrNum(err));
+			this_thread::sleep_for(milliseconds(3000)); // wait for probe to be sent
+		}
+		
+	}
+	else if ((type == 'b') || (type == 'B')) {
+		//pass these in as parameters
+		wchar_t COMportnum = L'7';	// check COM port in device manager
+		char modemtype = 'B';		// A for Alice, B for Bob
+		Modem bob(COMportnum, modemtype);	// configure bob
+
+		while (1) {
+			err = Bob(bob);
+			PrintError(ErrNum(err));
+		}		
 	}
 	else {
-		int64_t err = Bob();
-		PrintError(ErrNum(err));
+		return -1;
 	}
+
+
 }
